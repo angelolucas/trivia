7377593f1bea676a57777fefeeaf50dc90cb48b5
Angelo <angelo.lrc@gmail.com>
2020-10-29 21:16:43 -0300

    src folder with a Home.js

---
+++ src/screens/Home.js
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () => (
+  <View style={styles.container}>
+    <Text accessibilityRole="heading">Welcome to the Trivia Challenge!</Text>
+    <Text accessibilityRole="heading" aria-level="2">
+      You will be presented with 10 True of False Questions
+    </Text>
+    <Text accessibilityRole="heading" aria-level="3">
+      Can you score 100%
+    </Text>
+    <Text accessibilityRole="button">Begin</Text>
+    <StatusBar style="auto" />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Home;
