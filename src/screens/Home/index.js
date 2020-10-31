040e22df304c25666af112514622dd5b24bb8d38
Angelo <angelo.lrc@gmail.com>
2020-10-31 11:22:48 -0300

    Bringing content from API

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,27 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import * as S from "./styles";
-import data from "../../mock";
 
-const Home = ({ navigation }) => (
-  <S.Container>
-    <S.Text accessibilityRole="heading">
-      Welcome to the Trivia Challenge!
-    </S.Text>
-    <S.Text accessibilityRole="heading" aria-level="2">
-      You will be presented with 10 True of False Questions
-    </S.Text>
-    <S.Text accessibilityRole="heading" aria-level="3">
-      Can you score 100%
-    </S.Text>
-    <S.Text
-      accessibilityRole="button"
-      onPress={() => navigation.navigate("Quiz", { quizList: data.results })}
-    >
-      Begin
-    </S.Text>
-    <StatusBar style="auto" />
-  </S.Container>
-);
+const Home = ({ navigation }) => {
+  const fecthQuiz = fetch(
+    "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean"
+  ).then((response) => response.json());
+
+  const handleBegin = () => {
+    fecthQuiz.then((data) =>
+      navigation.navigate("Quiz", { quizList: data.results })
+    );
+  };
+
+  return (
+    <S.Container>
+      <S.Text accessibilityRole="heading">
+        Welcome to the Trivia Challenge!
+      </S.Text>
+      <S.Text accessibilityRole="heading" aria-level="2">
+        You will be presented with 10 True of False Questions
+      </S.Text>
+      <S.Text accessibilityRole="heading" aria-level="3">
+        Can you score 100%
+      </S.Text>
+      <S.Text accessibilityRole="button" onPress={handleBegin}>
+        Begin
+      </S.Text>
+      <StatusBar style="auto" />
+    </S.Container>
+  );
+};
 
 export default Home;
