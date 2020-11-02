bde47bd0aafd6e38b567d932484bc9246257f156
Angelo <angelo.lrc@gmail.com>
2020-11-02 16:57:47 -0300

    Text with align prop

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as S from './styles';
 
-const Text = ({ children, ...props }) => <S.Text {...props}>{children}</S.Text>;
+const Text = ({ children, align, ...props }) => (
+  <S.Text align={align} {...props}>
+    {children}
+  </S.Text>
+);
 
 Text.propTypes = {
   children: PropTypes.node.isRequired,
+  align: PropTypes.oneOf('left', 'center', 'right'),
+};
+
+Text.defaultProps = {
+  align: 'left',
 };
 
 export default Text;
--- src/components/Text/styles.js
+++ src/components/Text/styles.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components/native';
 
-export const Text = styled.Text(({ theme: { color, fontFamily } }) => ({
+export const Text = styled.Text(({ align, theme: { color, fontFamily } }) => ({
   color: color.foreground,
   fontSize: 18,
   fontFamily,
   marginBottom: 10,
+  textAlign: align,
 }));
